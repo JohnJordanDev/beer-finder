61e6d3c8fd2b75a3dc001b153e824eaf22439ecf
John Jordan <johnjordan1985@gmail.com>
2024-04-27 20:37:26 -0400

    add: flavor finder, w/ crisp_or_clean entries populated.

--- scripts/main.js
+++ scripts/main.js
@@ -12,15 +12,36 @@
     - Add "flavor" to main beer modal, based on mappings in an object (https://www.splendidtable.org/story/2013/03/21/the-7-flavor-categories-of-beer-what-they-are-how-to-pair-them)
 */
 const FLAVOR_TYPE = {
-    crisp_and_clean: {
-        delicate_fruit: [],
-        malt_accented: [],
-        brisk_hoppiness: []
+    crisp_or_clean: {
+        description: "Crisp beers are lighter, cleaner in flavor, and crisply refreshing. They range in color from straw to amber, have low to medium abvs, and are light to medium in body.",
+        subtypes: {
+            fruity: [
+                "Ale - Cream",
+                "Ale - English Blonde",
+                "Ale - American Blonde",
+                "Ale - Wheat",
+                "Kölsch"
+            ],
+            malty: [
+                "Bock - Helles",
+                "Lager - Pale",
+                "Lager - Helles",
+                "Lager - Amber",
+                "Lager - Vienna",
+                "Lager - Märzen"
+            ],
+            hoppy: [
+                "Pilsner",
+                "Pilsner - Imperial",
+                "Lager - Kellerbier/ Zwickelbier",
+                "Lager - India Pale"
+            ]
+        }
     },
-    hoppy_and_bitter: {},
-    malty_and_sweet: {},
-    roasted_and_smoked: {},
-    fruited_and_spiced: {},
+    hoppy_or_bitter: {},
+    malty_or_sweet: {},
+    roasted_or_smoked: {},
+    fruity_or_spiced: {},
     sour_and_funky: {}
 
 };
@@ -70,8 +91,11 @@ try {
     
 
 } catch(e) {
-
+    console.log(`Error during tab creation or execution: ${e.toString()}`);
 }
+
+let BEERS;
+
 ;(async function form (){
     try {
         const RATINGS = {
@@ -120,7 +144,7 @@ try {
             <p>Could not get beer list from server</p>`;
             return;
         }
-        const BEERS = (await dbReq.json());
+        BEERS = (await dbReq.json());
         console.log("Beers are: ", BEERS);
         console.log(Object.entries(BEERS).forEach(e => {
             const type = e[1].Rating;
@@ -257,5 +281,46 @@ try {
         m.innerText = e.toString();
         document.getElementById("search_form_wrapper").style.display = "none";
 
+    }
+}());
+
+;(async function(){
+    try {
+        const FLAVOR_RESULTS = document.getElementById("flavor_results_message");
+        // TODO: Add description to subtype and show in UI
+        const getFlavorEntries = (nameOfFlavor = "", flavor = {}) => {
+            let output = `<h4>${nameOfFlavor}</h4>
+                <p>${flavor.description}</p>`;
+            const subTypes = flavor.subtypes;
+            for(const subtype in subTypes){
+                output += `<h5>${subtype}</h5>
+                    <ul>`;
+                subTypes[subtype].forEach(st => {
+                    output += `<li>${st}</li>`;
+                });
+                output += "</ul>";
+            }
+            return output;
+        };
+        const flavorForm = `<form action="" method="GET">
+            <label class="radio"><input checked name="flavor" type="radio" value="crisp_or_clean">Crisp <small>or</small> Clean</label>
+            <label class="radio"><input name="flavor" type="radio" value="hoppy_or_bitter">Hoppy <small>or</small> Bitter</label>
+            <label class="radio"><input name="flavor" type="radio" value="malty_or_sweet">Malty <small>or</small> Sweet</label>
+            <label class="radio"><input name="flavor" type="radio" value="roasted_or_smoked">Roasted <small>or</small> Smoked</label>
+            <label class="radio"><input name="flavor" type="radio" value="fruity_or_spiced">Fruity <small>or</small> Spiced</label>
+            <label class="radio"><input name="flavor" type="radio" value="sour_or_funky">Sour <small>or</small> Funky</label>
+        </form>`;
+        const formWrapper = document.getElementById("flavor_form_wrapper");
+        formWrapper.innerHTML = flavorForm;
+        const formChangeHandler = ev => {
+            const option = ev.target;
+            FLAVOR_RESULTS.innerHTML = getFlavorEntries(option.value, FLAVOR_TYPE[option.value]);
+        };
+        formWrapper.querySelector("form").addEventListener("change", formChangeHandler);
+        const initiallyChecked = formWrapper.querySelector("[checked]");
+        FLAVOR_RESULTS.innerHTML = getFlavorEntries(initiallyChecked.value, FLAVOR_TYPE[initiallyChecked.value]);
+
+    } catch(e) {
+        console.log(`Error in Flavor Form: ${e.toString()}`);
     }
 }());
